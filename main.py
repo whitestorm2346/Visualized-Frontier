4c14f7e29ffd8ad686a6fea29c9fbe40df1273b6
whitestorm2346 <andrew770426@gmail.com>
2026-05-15 15:54:48 +0800

    Extended into a exploration algorithms visualizer. Added the manual.py, random_walk.py, lpfe.py. But LPFE algorithm need to optimize

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
+import argparse
 
 from config import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
+from exploration.lpfe import LPFEPolicy
 from world import World
 from occupancy_map import OccupancyMap
 from robot import Robot
 from renderer import Renderer
 from frontier import detect_frontiers
+
+from exploration.manual import ManualPolicy
+from exploration.random_walk import RandomWalkPolicy
 
 
 def handle_movement(keys, robot, world, dt):
@@ -37,7 +42,36 @@ def reset_simulation(robot, occupancy_map, world):
     occupancy_map.update_by_sensor(world, rx, ry)
 
 
+def create_policy(policy_name):
+    if policy_name == "manual":
+        return ManualPolicy()
+
+    if policy_name == "random":
+        return RandomWalkPolicy()
+    
+    if policy_name == "lpfe":
+        return LPFEPolicy()
+
+    raise ValueError(f"Unknown policy: {policy_name}")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-p",
+        "--policy",
+        choices=["manual", "random", "lpfe"],
+        default="manual",
+        help="Choose exploration policy"
+    )
+
+    return parser.parse_args()
+
+
 def main():
+    args = parse_args()
+
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -49,6 +83,8 @@ def main():
     occupancy_map = OccupancyMap(world.width, world.height)
     robot = Robot()
     renderer = Renderer(screen)
+
+    policy = create_policy(args.policy)
 
     rx, ry = robot.grid_pos()
     occupancy_map.update_by_sensor(world, rx, ry)
@@ -69,16 +105,20 @@ def main():
                     reset_simulation(robot, occupancy_map, world)
 
         keys = pygame.key.get_pressed()
+        frontiers = detect_frontiers(occupancy_map)
+
+        if hasattr(policy, "update_keys"):
+            policy.update_keys(keys)
+
+        dx, dy = policy.get_action(robot, occupancy_map, frontiers, world)
 
         old_pos = robot.grid_pos()
-
-        handle_movement(keys, robot, world, dt)
+        robot.try_move(dx, dy, world, dt)
 
         new_pos = robot.grid_pos()
         if new_pos != old_pos:
             occupancy_map.update_by_sensor(world, new_pos[0], new_pos[1])
 
-        frontiers = detect_frontiers(occupancy_map)
         renderer.draw(occupancy_map, frontiers, robot)
 
     pygame.quit()
